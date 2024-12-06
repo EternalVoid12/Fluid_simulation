89223a6b4f3c6fb876138c86aca1aab48b8611d0
EternalVoid12 <132580038+EternalVoid12@users.noreply.github.com>
2024-12-06 16:02:32 +0300

    All functions in solver now

--- main.cpp
+++ main.cpp
@@ -8,11 +8,10 @@
 #include <bitset>
 #include <climits>
 #include <cstring>
+#include "solver.h"
 
-#define IX(i,j) ((i)+(N+2)*(j))
-#define SWAP(x0,x) {float *tmp=x0;x0=x;x=tmp;}
 
-const int N = 100; // Grid size (100x100)
+const int N = 200; // Grid size (100x100)
 const int size = (N+2)*(N+2); // Total grid size with boundary
 
 static float u[size], v[size], u_prev[size], v_prev[size];
@@ -20,123 +19,7 @@ static float dens[size], dens_prev[size];
 static float s[size];
 
 
-void add_source ( int N, float * x, float * s, float dt )
-{
-    int i, size=(N+2)*(N+2);
-    for ( i=0 ; i<size ; i++ ) x[i] += dt*s[i]; //GENERATES DENSITY WITH TIME
-}
-void add_source_v ( int N, float * x, float * s, float dt )
-{
-    int i, size=(N+2)*(N+2);
-    for ( i=0 ; i<size ; i++ ) x[i] += dt*s[i];
-}
-void add_source_u ( int N, float * x, float * s, float dt )
-{
-    int i, size=(N+2)*(N+2);
-    for ( i=0 ; i<size ; i++ ) x[i] += dt*s[i];
-}
-void set_bnd ( int N, int b, float * x )
-{
-        int i;
-        for ( i=1 ; i<=N ; i++ ) {
-        x[IX(0 ,i)] = b==1 ? -x[IX(1,i)] : x[IX(1,i)];
-        x[IX(N+1,i)] = b==1 ? -x[IX(N,i)] : x[IX(N,i)];
-        x[IX(i,0 )] = b==2 ? -x[IX(i,1)] : x[IX(i,1)];
-        x[IX(i,N+1)] = b==2 ? -x[IX(i,N)] : x[IX(i,N)];
-        }
-        x[IX(0 ,0 )] = 0.5*(x[IX(1,0 )]+x[IX(0 ,1)]);
-        x[IX(0 ,N+1)] = 0.5*(x[IX(1,N+1)]+x[IX(0 ,N )]);
-        x[IX(N+1,0 )] = 0.5*(x[IX(N,0 )]+x[IX(N+1,1)]);
-        x[IX(N+1,N+1)] = 0.5*(x[IX(N,N+1)]+x[IX(N+1,N )]);
-}
-void diffuse ( int N, int b, float * x, float * x0, float diff, float dt )
-{
-    int i, j, k;
-    float a=dt*diff*N*N;
-    for ( k=0 ; k<20 ; k++ ) {
-    for ( i=1 ; i<=N ; i++ ) {
-    for ( j=1 ; j<=N ; j++ ) {
-        x[IX(i,j)] = (x0[IX(i,j)] + a*(x[IX(i-1,j)]+x[IX(i+1,j)]+
-        x[IX(i,j-1)]+x[IX(i,j+1)]))/(1+4*a);
-    }
-    }
-        set_bnd ( N, b, x );
-    }
-}
-void advect ( int N, int b, float * d, float * d0, float * u, float * v, float dt )
-{
-    int i, j, i0, j0, i1, j1;
-    float x, y, s0, t0, s1, t1, dt0;
-    dt0 = dt*N;
-    for ( i=1 ; i<=N ; i++ ) {
-        for ( j=1 ; j<=N ; j++ ) {
-        x = i-dt0*u[IX(i,j)]; y = j-dt0*v[IX(i,j)];
-        if (x<0.5) x=0.5; 
-        if (x>N+0.5) x=N+ 0.5; //TODO: CHEck if statements
-        i0=(int)x; 
-        i1=i0+1;
-        if (y<0.5) y=0.5; 
-        if (y>N+0.5) y=N+ 0.5; 
-        j0=(int)y; 
-        j1=j0+1;
-        s1 = x-i0; s0 = 1-s1; t1 = y-j0; t0 = 1-t1;
-        d[IX(i,j)] = s0*(t0*d0[IX(i0,j0)]+t1*d0[IX(i0,j1)])+ s1*(t0*d0[IX(i1,j0)]+t1*d0[IX(i1,j1)]);
-}
-}
-set_bnd ( N, b, d );
-}
-void dens_step ( int N, float * x, float * x0, float * u, float * v, float diff,
-                float dt )
-{
-    //add_source ( N, x, x0, dt );
-    SWAP(x0, x) // Ads density as it is not as source
-    SWAP ( x0, x ); diffuse ( N, 0, x, x0, diff, dt );
-    SWAP ( x0, x ); advect ( N, 0, x, x0, u, v, dt );
-}
 
-void project ( int N, float * u, float * v, float * p, float * div )
-{
-        int i, j, k;
-        float h;
-        h = 1.0/N;
-        for ( i=1 ; i<=N ; i++ ) {
-        for ( j=1 ; j<=N ; j++ ) {
-        div[IX(i,j)] = -0.5*h*(u[IX(i+1,j)]-u[IX(i-1,j)]+
-        v[IX(i,j+1)]-v[IX(i,j-1)]);
-        p[IX(i,j)] = 0;
-        }
-        }
-        set_bnd ( N, 0, div ); set_bnd ( N, 0, p );
-
-        for ( k=0 ; k<20 ; k++ ) {
-        for ( i=1 ; i<=N ; i++ ) {
-        for ( j=1 ; j<=N ; j++ ) {
-        p[IX(i,j)] = (div[IX(i,j)]+p[IX(i-1,j)]+p[IX(i+1,j)]+
-        p[IX(i,j-1)]+p[IX(i,j+1)])/4;
-        }
-        }
-        set_bnd ( N, 0, p );
-        }
-        for ( i=1 ; i<=N ; i++ ) {
-        for ( j=1 ; j<=N ; j++ ) {
-        u[IX(i,j)] -= 0.5*(p[IX(i+1,j)]-p[IX(i-1,j)])/h;
-        v[IX(i,j)] -= 0.5*(p[IX(i,j+1)]-p[IX(i,j-1)])/h;
-        }
-        }
-        set_bnd ( N, 1, u ); set_bnd ( N, 2, v );
-}
-
-void vel_step ( int N, float * u, float * v, float * u0, float * v0,
-float visc, float dt )
-{
-    add_source_u ( N, u, u0, dt ); add_source_v ( N, v, v0, dt );
-    SWAP ( u0, u ); diffuse ( N, 1, u, u0, visc, dt );
-    SWAP ( v0, v ); diffuse ( N, 2, v, v0, visc, dt );
-    project ( N, u, v, u0, v0 );
-    SWAP ( u0, u ); SWAP ( v0, v );
-    advect ( N, 1, u, u0, u0, v0, dt ); advect ( N, 2, v, v0, u0, v0, dt );
-    project ( N, u, v, u0, v0 );
-}
 void get_from_UI(float* s, float* u_prev, float* v_prev)
 {
     // Assume the simulation grid is N x N
@@ -292,11 +175,11 @@ int main(void)
     s[i] = 0.0f;
     }
 
+    Solver solver;
+
     const int screenWidth = 1000;
     const int screenHeight = 1000;
 
-
-    //const float dt = 0.1f;
     const float diffusion = 0.00001f;
     const float viscosity = 5.0f;
 
@@ -305,28 +188,18 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Fluid");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);               // Set our simulation to run at 60 frames-per-second
 
     float dt = 1.0f/60.0f;
     int timeStamp = 0;
-    //int stepCounter = 0;
 
-    std::random_device rd; // Источник случайности
-    std::mt19937 gen(rd()); // Генератор случайных чисел
+    //std::random_device rd; 
+    //std::mt19937 gen(rd());
     
-    std::uniform_real_distribution<float> dens_dis(0.0f, 100.0f); // Равномерное распределение
-    std::uniform_real_distribution<float> color_dis(-255.0f, 0.0f);
-    std::uniform_int_distribution<int> radius_dis(0, 50);
-    for (int i = 1; i <= N; i++) {
-    for (int j = 1; j <= N; j++) {
-    // Calculate the index for the current grid cell
-    int index = i + j * (N + 2); // +2 to account for boundary cells
-        //v[index]=color_dis(gen);
-        //u[index] = color_dis(gen);
-        //s[index] = dens_dis(gen);
+    //std::uniform_real_distribution<float> dens_dis(0.0f, 100.0f);
+    //std::uniform_real_distribution<float> color_dis(-255.0f, 0.0f);
+    //std::uniform_int_distribution<int> radius_dis(0, 50);
 
-    }
-    }
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
@@ -335,55 +208,39 @@ int main(void)
         // TODO: Update your variables here
         //----------------------------------------------------------------------------------
         timeStamp+=1;
-        ballPosition = GetMousePosition(); // IT AFFECTS V_PREV SOMEHOW!!!!NOt
-        //for (int i = 0; i < size; i++) v_prev[i]=200.0f; //draw newgative velocity too
-        for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-        // Calculate the index for the current grid cell
-            int index = i + j * (N + 2); // +2 to account for boundary cells
-            //v[index]=color_dis(gen);
-            //u[index] = color_dis(gen);
-            //dens_prev[index] = dens_dis(gen);
+        ballPosition = GetMousePosition(); // IT AFFECTS V_PREV SOMEHOW!!!!
 
-        }
-        }
-        //for (int i = 0; i < size; i++) v[i]=1000.0f;
         get_from_UI(dens_prev,u_prev, v_prev);
-
-        //for (int i = 0; i < size; i++) s[i]=0.0f;
-
-        vel_step(N, u, v, u_prev, v_prev, viscosity, dt);
-        dens_step(N, dens, dens_prev, u, v, diffusion, dt);
+        solver.vel_step(N, u, v, u_prev, v_prev, viscosity, dt);
+        solver.dens_step(N, dens, dens_prev, u, v, diffusion, dt);
         
-        //for (int i = 0; i < size; i++) s[i]=0.0f;
-        if (IsKeyPressed(KEY_A)){
-                //for(int i = 0; i < size; i++) std::cout<<s[i];
+        if (IsKeyPressed(KEY_A)) //Delete all densities
+        {
                 for(int i = 0; i < size; i++) s[i]=0.0f;
                 for(int i = 0; i < size; i++) dens[i]=0.0f;
                 for(int i = 0; i < size; i++) dens_prev[i]=0.0f;
-            }
+        }
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-
             ClearBackground(RAYWHITE);
 
-            if (IsKeyDown(KEY_SPACE)) {
+            if (IsKeyDown(KEY_SPACE)) 
+            {
                 draw_velocity(N,v);
-                //draw_forces(N,u);
             } else if(IsKeyDown(KEY_F)) 
             {
                 draw_forces(N,u);
-            } else{
+            } else
+            {
                 draw_dens(N,dens);
             }
+
             DrawCircleV(ballPosition, 5, ballColor);
             DrawFPS(10, 10); 
             std::string str = std::to_string(timeStamp);
             DrawText(str.c_str(),10,30,20, WHITE);
-            //DrawText(str.c_str(),20,20,10, WHITE);
-            //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
